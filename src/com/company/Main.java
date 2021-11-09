a7fb7c9c51b2a7649beca04e2672cd699ca42178
DavidP1996erea <david.perea@iesnervion.es>
2021-11-09 12:07:45 +0100

    primer commint

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("ASD");
+        System.out.println("asdklñgjnasdgjinsd");
 
 
     }
